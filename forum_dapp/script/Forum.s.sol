24288651668ae1523a7fffa4605df2e73f208e2c
victoriaEssien <victoriaessien857@gmail.com>
2024-09-20 06:51:31 +0100

    ready for deployment

---
+++ forum_dapp/script/Forum.s.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Forum} from "../src/Forum.sol";
+
+contract ForumScript is Script {
+    Forum public forum;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        forum = new Forum();
+
+        vm.stopBroadcast();
+    }
+}
